f4c69807524d051c21acce711972d945c4f808d0
Nicholas Smith <nsmith@ethosgroup.com>
2020-04-27 13:42:10 -0500

    Updated automatic updater

---
+++ automatic/hyperspace/update.ps1
@@ -0,0 +1,28 @@
+Import-Module au
+
+$releases = 'https://github.com/hyperspacedev/hyperspace/releases'
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyinstall.ps1" = @{
+            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
+            "(^*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'" 
+        }
+    }
+}
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $re = 'Hyperspace.Desktop.*.exe'
+    $url = $download_page.links | ? href -match $re | select -First 1 -expand href
+    $sourceUrl = 'https://github.com' + $url
+    $version = ($url -split '\/' | select -Index 5).Substring(1)
+
+    return @{
+        URL = $sourceUrl
+        Version = $version
+        ChecksumType32 = 'sha512'
+    }
+}
+
+Update-Package
