745a2ed4efced42cf551bec23a07c5f991600ff5
Nicholas Smith <nsmith@ethosgroup.com>
2020-05-08 12:58:13 -0500

    Added thedesk au file

---
+++ automatic/thedesk/update.ps1
@@ -0,0 +1,35 @@
+Import-Module au
+
+$releases = 'https://github.com/cutls/TheDesk/releases'
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyinstall.ps1" = @{
+            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
+            "(^[$]url64\s*=\s*)('.*')"  = "`${1} $($Latest.URL64)"
+            "(^*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'" 
+            "(?i)(checksum64:).*" = "`${1} $($Latest.Checksum64)"
+        }
+    }
+}
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $re = 'TheDesk-setup-ia32.exe'
+    $re64 = 'TheDesk-setup.exe'
+    $url = $download_page.links | ? href -match $re | select -First 1 -expand href
+    $url64 = $download_page.links | ? href -match $re64 | select -First 1 -expand href
+    $sourceUrl = 'https://github.com' + $url
+    $sourceUrl64 = 'https://github.com' + $url64
+    $version = ($url -split '\/' | select -Index 5).Substring(1)
+
+    return @{
+        URL = $sourceUrl
+        URL64 = $sourceUrl64
+        Version = $version
+        ChecksumType32 = 'sha512'
+        ChecksumType64 = 'sha512'
+    }
+}
+
+Update-Package
