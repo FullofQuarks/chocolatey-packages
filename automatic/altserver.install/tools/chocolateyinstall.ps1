27f5553dfc371776b39e01b4db5fb86bdcdbd9c8
Nicholas Smith <nsmith@ethosgroup.com>
2020-05-13 17:49:31 -0500

    Added altserver.install package

---
+++ automatic/altserver.install/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'AltInstaller.msi'
+$packageName = 'altserver'
+
+$installPackageArgs = @{
+  packageName   = $packageName
+  fileType      = 'MSI'
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  file          = $fileLocation
+  softwareName  = 'AltServer*'
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyInstallPackage @installPackageArgs
