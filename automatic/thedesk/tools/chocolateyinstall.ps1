3ef90b995ebf2bd6dd58593cfe6d9dcc6eec3fef
Nicholas Smith <nsmith@ethosgroup.com>
2020-05-18 11:36:25 -0500

    Upgrade thedesk to version 21.0.0

--- automatic/thedesk/tools/chocolateyinstall.ps1
+++ automatic/thedesk/tools/chocolateyinstall.ps1
@@ -2,10 +2,10 @@ $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageName = 'TheDesk'
 
-$url        = 'https://github.com/cutls/TheDesk/releases/download/v20.3.3/TheDesk-setup-ia32.exe'
-$url64      = 'https://github.com/cutls/TheDesk/releases/download/v20.3.3/TheDesk-setup.exe'
-$checksum   = ''
-$checksum64 =  ''
+$url        = 'https://github.com/cutls/TheDesk/releases/download/v21.0.0/TheDesk-setup-ia32.exe'
+$url64      = 'https://github.com/cutls/TheDesk/releases/download/v21.0.0/TheDesk-setup.exe'
+$checksum   = 'a969bc8e4a687197000417691f65f64a155bf5f05f404ea96c2f3e67b07a4e6d74a9bb1920bd8f24690af85a96c476a23e298cae884675cbffd1f235428db0e9'
+$checksum64 =  '101a23ae1526441f76d8693e778cf90419432802ed0b1de6e06b6c95597c2fca470f10f4738355108d1df5b9c1f31088570a82210b02ad8ff90bb0e86ef55a35'
 
 $packageArgs = @{
   packageName   = $packageName
