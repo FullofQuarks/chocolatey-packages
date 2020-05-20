d3b98d9af826e4e9d8481312ffd5ee3193695f8d
Nicholas Smith <nsmith@ethosgroup.com>
2020-05-20 11:29:41 -0500

    Update all script

--- update_all.ps1
+++ update_all.ps1
@@ -2,6 +2,10 @@
 
 param([string] $Name, [string] $ForcedPackages, [string] $Root = "$PSScriptRoot\automatic")
 
+# Set environment variables
+$Env:gist_id = Write-Host "Update all - $(Get-Date -Format "MM/dd/yyyy")"
+$Env:github_api_key = Get-Content .\api_key
+
 if (Test-Path $PSScriptRoot/update_vars.ps1) { . $PSScriptRoot/update_vars.ps1 }
 
 $Options = [ordered]@{
