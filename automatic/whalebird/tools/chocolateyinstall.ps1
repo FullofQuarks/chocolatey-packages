f53d13c63a39063e9d8ac98dd55d2d52664ba547
Nicholas Smith <nsmith@ethosgroup.com>
2020-06-01 10:23:49 -0500

    Update Whalebird to 4.1.2

--- automatic/whalebird/tools/chocolateyinstall.ps1
+++ automatic/whalebird/tools/chocolateyinstall.ps1
@@ -2,10 +2,10 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageName = 'whalebird'
 
-$url32        = 'https://github.com/h3poteto/whalebird-desktop/releases/download/4.1.1/Whalebird-4.1.1-windows-ia32.exe'
-$url64      = 'https://github.com/h3poteto/whalebird-desktop/releases/download/4.1.1/Whalebird-4.1.1-windows-x64.exe'
-$checksum   = 'a8d098fbbdfca48dc1a03ba89dc5efd3b93cc3eabf26764b0235fac91ed39d1557022c5f57881d8329a22e506559d16492cc6c5e1b4dcc19806fa8e188821eb8'
-$checksum64  = '3d289f045bb52a55de1de9037225e1d68004c2c2324ee1cfceb401580eeebd039c384c351a3a90abd2d9841eb79904fe0016bb3fe1464fc88ab8451856d57d25'
+$url32        = 'https://github.com/h3poteto/whalebird-desktop/releases/download/4.1.2/Whalebird-4.1.2-windows-ia32.exe'
+$url64      = 'https://github.com/h3poteto/whalebird-desktop/releases/download/4.1.2/Whalebird-4.1.2-windows-x64.exe'
+$checksum   = '6bfae5121434cd83f1b575e21bcb9637c011560f4f9ccb487f98e4b6668161c237a05c25dd26d5444cbd62e35965f693c686422bc295a495ecc523c4ae96774a'
+$checksum64  = '3c125d9be5d62fc70ab3af1392f00ddcdd084c4f6122fc4100c86822c1ba79c73020399d185dd860cc74ae0e253e2616bfbd920c740bfeb4cfe62a97800a9e9f'
 
 $packageArgs = @{
   packageName   = $packageName
