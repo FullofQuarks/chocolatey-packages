284973ea25996dc483a4c1e9e69a8801da0776bb
Nicholas Smith <nsmith@ethosgroup.com>
2020-06-17 09:24:53 -0500

    Update thedesk

--- automatic/thedesk/tools/chocolateyinstall.ps1
+++ automatic/thedesk/tools/chocolateyinstall.ps1
@@ -2,10 +2,10 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageName = 'TheDesk'
 
-$url        = 'https://github.com/cutls/TheDesk/releases/download/v21.0.1/TheDesk-setup-ia32.exe'
-$url64      = 'https://github.com/cutls/TheDesk/releases/download/v21.0.1/TheDesk-setup.exe'
-$checksum   = '5f4b3edefc4086c3810456b86168d3f802e96d61101269333e3159c375c1978a48b1820bec461f508d6eaeac9bf8966d0701365a7e406fe47161b4427bee2af0'
-$checksum64 =  '11eca0cba96b4fbae98fca599cdc2282b9769faf125239121c7a83d395bf2a4cdbe9a69413471244418184d7ae60838e4a68b7a16609e877944f1e9ceb7c9e04'
+$url        = 'https://github.com/cutls/TheDesk/releases/download/v21.0.3/TheDesk-setup-ia32.exe'
+$url64      = 'https://github.com/cutls/TheDesk/releases/download/v21.0.3/TheDesk-setup.exe'
+$checksum   = '70c93587ec75dbc95f070ddfd91ee0981c552adb1b238ae043b62e801e4cac801fc9ae2dd8cf761b3e7cc63caf8e981af7ea828c67786d15a08b2fe135e73db6'
+$checksum64 =  'c8a771ce75f44aaf88bc4c7b8868c1c9995d1f0723afae8b2b876340eabe4c92ae4d45ecc6d69f0a1aecb69c3c5ba01f6414296d0adbe18fad04a3719331382a'
 
 $packageArgs = @{
   packageName   = $packageName
