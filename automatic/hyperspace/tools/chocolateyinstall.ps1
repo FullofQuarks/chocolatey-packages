0b0d2c077ec95ed527cf63f72b972d3e54a43d85
Nicholas Smith <nsmith@ethosgroup.com>
2020-06-18 10:27:52 -0500

    Update Hyperspace 1.1.1 --> 1.1.2

--- automatic/hyperspace/tools/chocolateyinstall.ps1
+++ automatic/hyperspace/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/hyperspacedev/hyperspace/releases/download/v1.1.1/Hyperspace.Desktop.Setup.1.1.1.exe'
-$checksum   = '1954951aa6f93449918c4f217c3e9b1cf52b45495998a3054c156ec4e0a423edb274f69976c2b95a61696e256a7ec6592676aac9a1988bd5fc31b974d588e3a3'
+$url        = 'https://github.com/hyperspacedev/hyperspace/releases/download/v1.1.2/Hyperspace.Desktop.Setup.1.1.2.exe'
+$checksum   = '88d90b0b6a058266796fa170fa61e16e8dd7597c6934b0a8882d611147881d2875fc05e775623910ebf3340c8a4d61537d7c4d68df44cd9620a0bd7199436c88'
 $packageName = 'Hyperspace'
 
 
