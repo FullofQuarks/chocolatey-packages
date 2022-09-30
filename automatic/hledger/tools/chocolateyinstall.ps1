a2c940dc63104b57a8645e915eea47acd6d9dbba
Nicholas Smith <nsmith@ethosgroup.com>
2022-09-30 10:29:12 -0500

    Update hledger

--- automatic/hledger/tools/chocolateyinstall.ps1
+++ automatic/hledger/tools/chocolateyinstall.ps1
@@ -1,12 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/simonmichael/hledger/releases/download/1.24/hledger-windows.zip'
+$url        = 'https://github.com/simonmichael/hledger/releases/download/1.25/hledger-windows.zip'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   url           = $url
-  checksum      = 'ef31de7a3b5ecb67e71c49089f571e7c3206c75ea77124cf659bcb0ec603ed62'
+  checksum      = '19838b6e732ed10ee88b64aa806247a5c7b7c5d3266d804288972ce588e2e426'
   checksumType  = 'sha256'
 }
 
