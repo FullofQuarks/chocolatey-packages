1c4475f1de1931b6e31c21e237495720cbab80ef
Nicholas Smith <nsmith@ethosgroup.com>
2023-04-05 16:36:36 -0500

    coolterm: corrected 32 bit hash

--- automatic/coolterm/tools/chocolateyinstall.ps1
+++ automatic/coolterm/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 $packagename = 'coolterm'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url32 = 'https://freeware.the-meiers.org/CoolTermWin32Bit.zip'
-$checksum32 = '0f12af99ed59fd016849006587269df9a0cc361d081c3cac7dfd8522fbad4b1da25c41e4781ff28361692a95e2406082d20345228fd5d54db4ca70f9d2068186'
+$checksum32 = 'a239867abdd9be5c44b2ba5dbfb62ad774732fa7c23cdfd877f665803429158a075993620ffa6d45f3b80eb8f27e686d2d82e8366104f86bae2c956b7d3e2498'
 $url64 = 'https://freeware.the-meiers.org/CoolTermWin64Bit.zip'
 $checksum64 = 'e1453b5c52d284233c2b3a6a9e690056e4007d9925db641b1bcdf8c4314ce8a00237c64ad2ad58fd1ec122a393b6bff9df0410568c9c3335a647370adff118e2'
 $checksumType = 'sha512'
