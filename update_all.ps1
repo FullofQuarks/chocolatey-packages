4d3c67fe2b5addd50607688f1502377797b1a946
Nicholas Smith <nicholassmith32@icloud.com>
2023-04-07 01:20:51 -0500

    Updated api key secret name

--- update_all.ps1
+++ update_all.ps1
@@ -35,7 +35,7 @@ $Options = [ordered]@{
 
     Git = @{
         User     = ''                                       #Git username, leave empty if github api key is used
-        Password = $Env:github_api_key                      #Password if username is not empty, otherwise api key
+        Password = $Env:API_KEY                             #Password if username is not empty, otherwise api key
     }
 
     RunInfo = @{
