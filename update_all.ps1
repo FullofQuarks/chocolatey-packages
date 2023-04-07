e4cb56b1957ba353e52fc175b04a9ee0c05cabbd
Nicholas Smith <seeya32@gmail.com>
2023-04-07 00:25:12 -0500

    Get rid of mail and gist

--- update_all.ps1
+++ update_all.ps1
@@ -33,12 +33,6 @@ $Options = [ordered]@{
         Path = "$PSScriptRoot\Update-History.md"            #Path where to save history
     }
 
-    Gist = @{
-        Id     = $Env:gist_id                               #Your gist id; leave empty for new private or anonymous gist
-        ApiKey = $Env:github_api_key                        #Your github api key - if empty anoymous gist is created
-        Path   = "$PSScriptRoot\Update-AUPackages.md", "$PSScriptRoot\Update-History.md"       #List of files to add to the gist
-    }
-
     Git = @{
         User     = ''                                       #Git username, leave empty if github api key is used
         Password = $Env:github_api_key                      #Password if username is not empty, otherwise api key
@@ -48,20 +42,6 @@ $Options = [ordered]@{
         Exclude = 'password', 'apikey'                      #Option keys which contain those words will be removed
         Path    = "$PSScriptRoot\update_info.xml"           #Path where to save the run info
     }
-
-    Mail = if ($Env:mail_user) {
-            @{
-                To         = $Env:mail_user
-                Server     = $Env:mail_server
-                UserName   = $Env:mail_user
-                Password   = $Env:mail_pass
-                Port       = $Env:mail_port
-                EnableSsl  = $Env:mail_enablessl -eq 'true'
-                Attachment = "$PSScriptRoot\update_info.xml"
-                UserMessage = ''
-                SendAlways  = $false                        #Send notifications every time
-             }
-           } else {}
 
     ForcedPackages = $ForcedPackages -split ' '
     BeforeEach = {
