7d8fc5d4e65255bc7b7c4705f2989a4891c73d47
Nicholas Smith <nsmith@ethosgroup.com>
2023-05-26 11:52:38 -0500

    Whalebird removed 32bit

--- automatic/whalebird/tools/chocolateyinstall.ps1
+++ automatic/whalebird/tools/chocolateyinstall.ps1
@@ -2,21 +2,16 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageName = 'whalebird'
 
-$url32        = 'https://github.com/h3poteto/whalebird-desktop/releases/download/v5.0.4/Whalebird-5.0.4-windows-ia32.exe'
 $url64      = 'https://github.com/h3poteto/whalebird-desktop/releases/download/v5.0.4/Whalebird-5.0.4-windows-x64.exe'
-$checksum   = '285e96c834a73f19fd126e89f6828747f82440da377f2faa9b72ccf0cc1e9acb2a26ef8d524e7b79ccb9bbe18bc6075c417c2a09dd841862e9488f37a535133a'
 $checksum64  = 'f94c50d79c95c8f5a9680a780eb3ffccb336b927cc068ac07f0035c3b55f309703edba6544ce7c3a2532c4dd4f5ae69df88203368e85441012f35231029f4089'
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
-  url           = $url32
   url64bit      = $url64
 
   softwareName  = 'whalebird*'
-  checksum      = $checksum
-  checksumType32  = 'sha512'
   checksum64    = $checksum64
   checksumType64= 'sha512'
 
--- automatic/whalebird/update.ps1
+++ automatic/whalebird/update.ps1
@@ -3,35 +3,27 @@ Import-Module au
 $releases = 'https://api.github.com/repos/h3poteto/whalebird-desktop/releases'
 
 function global:au_BeforeUpdate() {
-    $Latest.Checksum32 = Get-RemoteChecksum -Algorithm sha512 $Latest.URL
     $Latest.Checksum64 = Get-RemoteChecksum -Algorithm sha512 $Latest.URL64
 }
 
 function global:au_SearchReplace {
-    Write-Host($Latest.Checksum32)
     @{
         "tools\chocolateyinstall.ps1" = @{
-            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
             "(^[$]url64\s*=\s*)('.*')"  = "`$1'$($Latest.URL64)'"
-            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'" 
             "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }
     }
 }
 function global:au_GetLatest {
     $latestReleases_json = Invoke-RestMethod -Uri $releases
-    $windowsAsset = $latestReleases_json[0].assets | where { ($_.name -like "*windows*ia32*") -and ($_.name -notlike "*shasum*")}
-    $windowsAsset64 = $latestReleases_json[0].assets | where { ($_.name -like "*windows*x64*") -and ($_.name -notlike "*shasum*")}
+    $windowsAsset64 = $latestReleases_json[0].assets | where { ($_.name -like "*win*x64*") -and ($_.name -notlike "*shasum*") -and ($_.name -notlike "*.blockmap")}
 
-    $url = $windowsAsset.browser_download_url
     $url64 = $windowsAsset64.browser_download_url
     $version = $latestReleases_json[0].name
 
     return @{
-        URL = $url
         URL64 = $url64
         Version = $version
-        ChecksumType32 = 'sha512'
         ChecksumType64 = 'sha512'
     }
 }
