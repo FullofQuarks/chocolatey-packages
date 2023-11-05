0f84ac945a6a675cf1e18fcb1980fb81e764d95a
Chocolatey Automatic Updater <choco@someemail.com>
2023-11-05 01:19:29 +0000

    AU: 2 updated - thedesk thedesk.portable [skip ci]

--- automatic/thedesk.portable/tools/chocolateyinstall.ps1
+++ automatic/thedesk.portable/tools/chocolateyinstall.ps1
@@ -2,10 +2,10 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageName = 'TheDesk'
 
-$url        = 'https://github.com/cutls/TheDesk/releases/download/v24.1.3/TheDesk-ia32.exe'
-$url64      = 'https://github.com/cutls/TheDesk/releases/download/v24.1.3/TheDesk.exe'
-$checksum   = 'babdd56375eff12c3fdaec84a9f2e3f318d33be19d25a1d589deaf98ab6c554c72d5178b3ff9a1cc31a455f5fcb58a973f86e21ddfb4de8441b9a4db13de8915'
-$checksum64 =  '8a171e7755633480cbe536d164f0b89791ace6ae42b12ddf681c84a8a734671c353c79b785648c12aa25d7229bc205bc483a315a65814002bfe4f03fba343dc5'
+$url        = 'https://github.com/cutls/TheDesk/releases/download/v24.1.5/TheDesk-ia32.exe'
+$url64      = 'https://github.com/cutls/TheDesk/releases/download/v24.1.5/TheDesk.exe'
+$checksum   = 'cb1ed762a00f082cc00b2370d5a155befe891023d592cf86e308be709e472398f5b85907a9d404da9ca9ef568f6e1644bc83b1cf06559dfc4dd9df9842d7da3a'
+$checksum64 =  'cce0212359ca9b511d20c3c7b1af8790d458b568a97ba7c03cafd4c8808959bb3cbe7d6a2cef49da67be8c0c218e6a721e63e956db405e0bf2cad015a5bb293b'
 
 $packageArgs = @{
   packageName   = $packageName
--- automatic/thedesk/tools/chocolateyinstall.ps1
+++ automatic/thedesk/tools/chocolateyinstall.ps1
@@ -2,10 +2,10 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageName = 'TheDesk'
 
-$url        = 'https://github.com/cutls/TheDesk/releases/download/v24.1.3/TheDesk-setup-ia32.exe'
-$url64      = 'https://github.com/cutls/TheDesk/releases/download/v24.1.3/TheDesk-setup.exe'
-$checksum   = '5a8fcf7b9ea772365bb14d3b713aa033eb6b47d3c3b10c3682cd2d04f6e58cf3bc0d873484f04f7fdd32253c17f5755260efd5324065605b9bc14d41c8258bea'
-$checksum64 =  '7a215c5840ef76b32cf73013cde71112350e587dce7c29ab1eda133581138576b8f485114dc84646d50a08e1b49778305e6d90196706ce75e7aa119af0dbd5bc'
+$url        = 'https://github.com/cutls/TheDesk/releases/download/v24.1.5/TheDesk-setup-ia32.exe'
+$url64      = 'https://github.com/cutls/TheDesk/releases/download/v24.1.5/TheDesk-setup.exe'
+$checksum   = 'ff67236bb0e5794b96587bd205343f4e2de2b0155d5590be0210442fba7cbeba9ecbc9f40aae029ce20eb29af9efb1e52279a0b98c05cbb905fe19b06e695da9'
+$checksum64 =  'de417207992cdcd60023acd1478c741e2a9212264b6f9cf98f1d76ac4053edd2ba8b856764a46eebe324d1ff7b164b391d38d8ddb92edb9791213e136e1b3689'
 
 $packageArgs = @{
   packageName   = $packageName
