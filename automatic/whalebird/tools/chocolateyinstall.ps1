a57dcf3a3aef28a00df8868a5ed950f38838a86d
Chocolatey Automatic Updater <choco@someemail.com>
2024-01-31 01:13:30 +0000

    AU: 1 updated - whalebird [skip ci]

--- automatic/whalebird/tools/chocolateyinstall.ps1
+++ automatic/whalebird/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageName = 'whalebird'
 
-$url64      = 'https://github.com/h3poteto/whalebird-desktop/releases/download/v6.0.0/Whalebird-6.0.0-win-x64.exe'
-$checksum64  = 'c61420fb7849102bfd8d53d13f326ffa47bdda613a51d3d427ef049cd24b110f85f998b0f5f33965580dcd90d4f8fd9e49bbe889fb4acf00b6143c7cd00c6dfb'
+$url64      = 'https://github.com/h3poteto/whalebird-desktop/releases/download/v6.0.1/Whalebird-6.0.1-win-x64.exe'
+$checksum64  = '564224eed99e8bb7c2b74a9b047081f65f1f8aafde93eb539b8781d71396e9359ec992d539268e484420ce465f909a9a7272445e3767d273d7ea1ffd11227efa'
 
 $packageArgs = @{
   packageName   = $packageName
