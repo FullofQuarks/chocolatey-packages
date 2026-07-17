d943e62d81b50d5c3ca893b2192e008346818574
Nicholas Smith <nicholas.smith4@utdallas.edu>
2026-07-17 18:42:54 -0500

    Try to fix ledger

--- automatic/ledger/update.ps1
+++ automatic/ledger/update.ps1
@@ -1,29 +1,63 @@
-Import-Module au
-Import-Module -name ..\..\tools\Get-LatestRelease
+Import-Module Chocolatey-AU -ErrorAction Stop
 
-$releases = 'https://api.github.com/repos/FullofQuarks/Windows-Ledger-Binaries/releases'
+$releases = 'https://api.github.com/repos/FullofQuarks/Windows-Ledger-Binaries/releases/latest'
+
+function Get-GitHubHeaders {
+    $headers = @{
+        Accept               = 'application/vnd.github+json'
+        'User-Agent'         = 'FullofQuarks-Chocolatey-AU'
+        'X-GitHub-Api-Version' = '2022-11-28'
+    }
+
+    if ($env:GITHUB_TOKEN) {
+        $headers.Authorization = "Bearer $env:GITHUB_TOKEN"
+    }
+
+    return $headers
+}
 
 function global:au_BeforeUpdate {
-    $Latest.Checksum32 = Get-RemoteChecksum -Algorithm sha512 $Latest.URL
+    $Latest.Checksum32 = Get-RemoteChecksum `
+        -Algorithm sha256 `
+        -Url $Latest.URL
 }
+
 function global:au_SearchReplace {
     @{
-        "tools\chocolateyinstall.ps1" = @{
-            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
-            "(checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'" 
+        'tools\chocolateyinstall.ps1' = @{
+            "(^[$]url\s*=\s*)('.*')" =
+                "`$1'$($Latest.URL)'"
+
+            "(^[$]checksum\s*=\s*)('.*')" =
+                "`$1'$($Latest.Checksum32)'"
+
+            "(^[$]checksumType\s*=\s*)('.*')" =
+                "`$1'sha256'"
         }
     }
 }
+
 function global:au_GetLatest {
-    $latestRelease_json = Invoke-RestMethod -Uri $releases
-    $sourceUrl = $latestRelease_json[0].assets.browser_download_url
-    $version = $latestRelease_json[0].name
-    
+    $release = Invoke-RestMethod `
+        -Uri $releases `
+        -Headers (Get-GitHubHeaders) `
+        -ErrorAction Stop
+
+    $asset = $release.assets |
+        Where-Object { $_.name -eq 'ledger.exe' } |
+        Select-Object -First 1
+
+    if (-not $asset) {
+        throw "The latest release '$($release.tag_name)' does not contain ledger.exe."
+    }
+
+    $version = $release.tag_name -replace '^v', ''
+
     return @{
-        URL = $sourceUrl
-        Version = $version
-        ChecksumType32 = 'sha512'
+        URL            = $asset.browser_download_url
+        Version        = $version
+        ChecksumType32 = 'sha256'
     }
 }
 
-update -ChecksumFor none
+Update-Package -ChecksumFor none
