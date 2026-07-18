50a0d98ff0c2eea3c74f2af8eff7ce2f424c8bd8
Nicholas Smith <nicholas.smith4@utdallas.edu>
2026-07-17 19:29:25 -0500

    changed regex

--- automatic/ledger/update.ps1
+++ automatic/ledger/update.ps1
@@ -1,25 +1,12 @@
-Import-Module AU -ErrorAction Stop
+Import-Module AU -RequiredVersion 2022.10.24 -ErrorAction Stop
 
-$releases = 'https://api.github.com/repos/FullofQuarks/Windows-Ledger-Binaries/releases/latest'
-
-function Get-GitHubHeaders {
-    $headers = @{
-        Accept               = 'application/vnd.github+json'
-        'User-Agent'         = 'FullofQuarks-Chocolatey-AU'
-        'X-GitHub-Api-Version' = '2022-11-28'
-    }
-
-    if ($env:GITHUB_TOKEN) {
-        $headers.Authorization = "Bearer $env:GITHUB_TOKEN"
-    }
-
-    return $headers
-}
+$releasesUrl =
+    'https://api.github.com/repos/FullofQuarks/Windows-Ledger-Binaries/releases/latest'
 
 function global:au_BeforeUpdate {
     $Latest.Checksum32 = Get-RemoteChecksum `
-        -Algorithm sha256 `
-        -Url $Latest.URL
+        -Url $Latest.URL `
+        -Algorithm sha256
 }
 
 function global:au_SearchReplace {
@@ -28,10 +15,10 @@ function global:au_SearchReplace {
             "(^[$]url\s*=\s*)('.*')" =
                 "`$1'$($Latest.URL)'"
 
-            "(^[$]checksum\s*=\s*)('.*')" =
+            "(^\s*checksum\s*=\s*)('.*')" =
                 "`$1'$($Latest.Checksum32)'"
 
-            "(^[$]checksumType\s*=\s*)('.*')" =
+            "(^\s*checksumType\s*=\s*)('.*')" =
                 "`$1'sha256'"
         }
     }
@@ -39,24 +26,20 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
     $release = Invoke-RestMethod `
-        -Uri $releases `
-        -Headers (Get-GitHubHeaders) `
-        -ErrorAction Stop
+        -Uri $releasesUrl `
+        -Headers @{ 'User-Agent' = 'Chocolatey-AU' }
 
     $asset = $release.assets |
         Where-Object { $_.name -eq 'ledger.exe' } |
         Select-Object -First 1
 
     if (-not $asset) {
-        throw "The latest release '$($release.tag_name)' does not contain ledger.exe."
+        throw "Release '$($release.tag_name)' does not contain ledger.exe."
     }
 
-    $version = $release.tag_name -replace '^v', ''
-
-    return @{
-        URL            = $asset.browser_download_url
-        Version        = $version
-        ChecksumType32 = 'sha256'
+    @{
+        URL     = $asset.browser_download_url
+        Version = $release.tag_name -replace '^v', ''
     }
 }
 
