447814f4f58b98697d492aed9125cd2e74d4ef99
Nicholas Smith <nicholas.smith4@utdallas.edu>
2026-08-07 18:41:53 -0500

    Updated hledger to not include pre-release

--- automatic/hledger/update.ps1
+++ automatic/hledger/update.ps1
@@ -5,23 +5,29 @@ $releases = 'https://api.github.com/repos/simonmichael/hledger/releases'
 function global:au_BeforeUpdate {
     $Latest.Checksum32 = Get-RemoteChecksum -Algorithm sha256 $Latest.URL
 }
+
 function global:au_SearchReplace {
     @{
         "tools\chocolateyinstall.ps1" = @{
-            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
-            "(checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'" 
+            "(^[$]url\s*=\s*)('.*')" = "`$1'$($Latest.URL)'"
+            "(checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
         }
     }
 }
+
 function global:au_GetLatest {
-    $latestRelease_json = Invoke-RestMethod -Uri $releases
-    $windowsAsset = $latestRelease_json[0].assets | where { $_.name -eq "hledger-windows-x64.zip" }
-    $sourceUrl = $windowsAsset.browser_download_url
-    $version = $latestRelease_json[0].name
+    $releasesJson = Invoke-RestMethod -Uri $releases
+
+    $latestRelease = $releasesJson |
+        Where-Object { -not $_.prerelease -and -not $_.draft } |
+        Select-Object -First 1
+
+    $windowsAsset = $latestRelease.assets |
+        Where-Object { $_.name -eq "hledger-windows-x64.zip" }
 
     return @{
-        URL = $sourceUrl
-        Version = $version
+        URL            = $windowsAsset.browser_download_url
+        Version        = $latestRelease.tag_name
         ChecksumType32 = 'sha256'
     }
 }
