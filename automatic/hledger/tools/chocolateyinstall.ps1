0d4f0b0cf80d35c81438f02436ae4ee048a68c37
Chocolatey Automatic Updater <choco@someemail.com>
2026-08-25 01:21:34 +0000

    AU: 1 updated - hledger [skip ci]

--- automatic/hledger/tools/chocolateyinstall.ps1
+++ automatic/hledger/tools/chocolateyinstall.ps1
@@ -1,12 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/simonmichael/hledger/releases/download/1.99.3/hledger-windows-x64.zip'
+$url        = 'https://github.com/simonmichael/hledger/releases/download/1.52.2/hledger-windows-x64.zip'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   url           = $url
-  checksum      = '2c06d7abde6de04792dd48409c8d36eb2d5d6314a9e13d090eb99de47f06c6fb'
+  checksum      = '94ad023807407b28b70a3fa00f794a3abed5890b59d36b3baa46aa24d5085ebe'
   checksumType  = 'sha256'
 }
 
